b285b04a99336ec0c11e957c65047cfdaed287dd
HishD <hisharadilshan3@gmail.com>
2024-06-23 04:35:13 +0100

    Added tests

--- Tests/DependencyInjectorTests/DependencyInjectorTests.swift
+++ Tests/DependencyInjectorTests/DependencyInjectorTests.swift
@@ -2,11 +2,43 @@ import XCTest
 @testable import DependencyInjector
 
 final class DependencyInjectorTests: XCTestCase {
+    
+    @Injectable(\.dataLogger) var logger: Logger 
+    @Injectable(\.mockLogger) var mock: Logger
+    
     func testExample() throws {
         // XCTest Documentation
         // https://developer.apple.com/documentation/xctest
 
         // Defining Test Cases and Test Methods
         // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
+
+struct DataLoggerDependency: InjectableDependency {
+    static var dependency: Logger = DataLogger()
+}
+
+struct MockLoggerDependency: InjectableDependency {
+    static var dependency: Logger = MockLogger()
+}
+
+extension InjectableValues {
+    var dataLogger: Logger {
+        get {
+            Self[DataLoggerDependency.self]
+        }
+        set {
+            Self[DataLoggerDependency.self] = newValue
+        }
+    }
+    
+    var mockLogger: Logger {
+        get {
+            Self[MockLoggerDependency.self]
+        }
+        set {
+            Self[MockLoggerDependency.self] = newValue
+        }
     }
 }
